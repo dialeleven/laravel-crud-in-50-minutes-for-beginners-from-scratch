ea1a40d0accec40a5d86306b5900c95bc587ff6d
Francis Tsao <dialeleven@gmail.com>
2024-04-24 14:48:34 -0400

    route/controller/view for product index and create product

--- app-crud/routes/web.php
+++ app-crud/routes/web.php
@@ -7,4 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/product', [ProductController::class, 'index'])->name('products.index');
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
