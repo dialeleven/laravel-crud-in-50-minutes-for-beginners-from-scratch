610bbb78e41472b9f2b4bb4adec47a49de43c880
Francis Tsao <dialeleven@gmail.com>
2024-04-24 20:20:41 -0400

    create

--- app-crud/app/Http/Controllers/ProductController.php
+++ app-crud/app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+
 
 class ProductController extends Controller
 {
@@ -12,5 +14,24 @@ class ProductController extends Controller
 
     public function create() {
         return view('products.create');
+    }
+
+    public function store(Request $request) {
+        // dump and die function to dump the $request data to the browser
+        //dd('description: ' . $request->description);
+        //dd($request);
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:2', // 2 decimal places
+            'description' => 'required',
+        ]);
+
+        // save request data to database
+        $newProduct = Product::create($data);
+
+        // redirect to product index page after
+        return redirect(route('product.index'));
+
     }
 }
--- app-crud/resources/views/products/create.blade.php
+++ app-crud/resources/views/products/create.blade.php
@@ -7,5 +7,28 @@
 </head>
 <body>
    <h1>Create a Product</h1>
+   <form method="post" action="{{route('product.store')}}">
+      @csrf
+      @method('post')
+      <div>
+         <label for="">Name</label>
+         <input type="text" name="name" placeholder="Name" />
+      </div>
+      <div>
+         <label for="">Qty</label>
+         <input type="text" name="qty" placeholder="Qty" />
+      </div>
+      <div>
+         <label for="">Price</label>
+         <input type="text" name="price" placeholder="Price" />
+      </div>
+      <div>
+         <label for="">Description</label>
+         <input type="text" name="description" placeholder="Description" />
+      </div>
+      <div>
+         <input type="submit" value="Save a New Product" />
+      </div>
+   </form>
 </body>
 </html>
