33453e975d8c2102f0b90263cd8fcd9a0a20fe67
Francis Tsao <dialeleven@gmail.com>
2024-04-30 16:50:01 -0400

    moved styles into assets folder finally

--- app-crud/resources/views/products/create.blade.php
+++ app-crud/resources/views/products/create.blade.php
@@ -5,63 +5,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Product Create</title>
    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-   <!--<link rel="stylesheet" href="/assets/css/style_product.css">-->
-   <style type="text/css">
-      body {
-         font: 14px Roboto;
-      }
+   <link rel="stylesheet" href="{{asset('assets/css/product_create.css')}}">
 
-      form {
-         max-width: 400px;
-         margin: 0 auto;
-         padding: 20px;
-         border: 1px solid #ccc;
-         border-radius: 5px;
-      }
-
-      label {
-         display: block;
-         margin-bottom: 10px;
-      }
-
-      input[type="text"],
-      input[type="email"] {
-         width: 95%;
-         padding: 10px;
-         margin-bottom: 20px;
-         border: 1px solid #ccc;
-         border-radius: 5px;
-      }
-
-      input[type='submit'] {
-         background-color: #0056b3;
-         border: 1px solid #000000;
-         color: #eee;
-         padding: 5px;
-         border-radius: 5px;
-         transition: background-color 0.2s ease; /* Smooth transition */
-      }
-
-      input[type="submit"]:hover {
-         background-color: #2189f8; /* New background color on hover */
-      }
-
-
-      /* Cancel button styles */
-      .cancel-button {
-            background-color: #b3b3b3; /* Red background color */
-            color: #000000; /* Default text color */
-            padding: 5px;
-            border-radius: 5px;
-            border: 1px solid #000000;
-            transition: background-color 0.2s ease; /* Smooth transition */
-        }
-
-        /* Hover effect for cancel button */
-        .cancel-button:hover {
-            background-color: #dc5361; /* Darker red on hover */
-        }
-   </style>
 </head>
 <body>
 
--- app-crud/resources/views/products/index.blade.php
+++ app-crud/resources/views/products/index.blade.php
@@ -5,61 +5,11 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Product Index</title>
    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-   <style type="text/css">
-      body, th, td {
-         font: 14px Roboto;
-      }
-
-      label {
-         display: block;
-         margin-bottom: 10px;
-      }
-
-      input[type="text"],
-      input[type="email"] {
-         width: 95%;
-         padding: 10px;
-         margin-bottom: 20px;
-         border: 1px solid #ccc;
-         border-radius: 5px;
-      }
-
-      
-      /* delete button styles */
-      .delete-button {
-            background-color: #e17a7a; /* Red background color */
-            color: #000000; /* Default text color */
-            padding: 5px;
-            border-radius: 5px;
-            border: 1px solid #000000;
-            transition: background-color 0.2s ease; /* Smooth transition */
-        }
-
-        /* Hover effect for cancel button */
-        .delete-button:hover {
-            background-color: #dc5361; /* Darker red on hover */
-        }
-
-
-      /* Cancel button styles */
-      .cancel-button {
-            background-color: #b3b3b3; /* Red background color */
-            color: #000000; /* Default text color */
-            padding: 5px;
-            border-radius: 5px;
-            border: 1px solid #000000;
-            transition: background-color 0.2s ease; /* Smooth transition */
-        }
-
-        /* Hover effect for cancel button */
-        .cancel-button:hover {
-            background-color: #dc5361; /* Darker red on hover */
-        }
-   </style>
+   <link rel="stylesheet" href="{{asset('assets/css/product_index.css')}}">
 </head>
 <body>
    <h1>Products</h1>
-   
+
    <p><a href="{{route('product.create')}}">Add a Product</a></p>
    <div>
       @if(@session()->has('success'))
