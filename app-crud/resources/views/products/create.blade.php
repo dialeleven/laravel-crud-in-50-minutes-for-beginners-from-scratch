5fac42251e9e2752278f84108f80cfc22ac05d54
Francis Tsao <dialeleven@gmail.com>
2024-05-02 20:05:53 -0400

    validation formatting

--- app-crud/resources/views/products/create.blade.php
+++ app-crud/resources/views/products/create.blade.php
@@ -9,36 +9,35 @@
 
 </head>
 <body>
-
-   <div>
-      @if ($errors->any())
-      <ul>
-         @foreach($errors->all() as $error)
-            <li>{{$error}}</li>
-         @endforeach
-      </ul>
-      @endif
-   </div>
    
    <form method="post" action="{{route('product.store')}}">
       @csrf
       @method('post')
       <h1>Create a Product</h1>
       <div>
+         @if ($errors->any())
+         <ul class="error-list">
+            @foreach($errors->all() as $error)
+               <li class="error-list-item">{{$error}}</li>
+            @endforeach
+         </ul>
+         @endif
+      </div>
+      <div>
          <label">Name</label>
-         <input type="text" name="name" placeholder="Name" value="{{old('name')}}" />
+         <input type="text" id="name" name="name" placeholder="Name" value="{{old('name')}}" />
       </div>
       <div>
          <label>Qty</label>
-         <input type="text" name="qty" placeholder="Qty" value="{{old('qty')}}" />
+         <input type="text" id="qty" name="qty" placeholder="Qty" value="{{old('qty')}}" />
       </div>
       <div>
          <label>Price</label>
-         <input type="text" name="price" placeholder="Price" value="{{old('price')}}" />
+         <input type="text" id="price" name="price" placeholder="Price" value="{{old('price')}}" />
       </div>
       <div>
          <label>Description</label>
-         <input type="text" name="description" placeholder="Description" value="{{old('description')}}" />
+         <input type="text" id="description" name="description" placeholder="Description" value="{{old('description')}}" />
       </div>
       <div>
          <label>Image</label>
