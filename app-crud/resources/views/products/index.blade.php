7470b889b703da7f2d8189116b05a946ded6150a
Francis Tsao <dialeleven@gmail.com>
2024-05-09 00:33:23 -0400

    Tailwind CSS added to product index view

--- app-crud/resources/views/products/index.blade.php
+++ app-crud/resources/views/products/index.blade.php
@@ -7,8 +7,8 @@
    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
    <!-- Include Font Awesome CSS --> 
    <link href="{{asset('assets/fontawesome/css/all.min.css')}}" rel="stylesheet" />
-   <link rel="stylesheet" href="{{asset('assets/css/product_index.css')}}">
-   <!--@vite('resources/css/app.css')-->
+   <!--<link rel="stylesheet" href="{{asset('assets/css/product_index.css')}}">-->
+   @vite('resources/css/app.css')
    <script>
       // Function to remove the status message after a timeout
       function removeStatusMessage() {
@@ -25,67 +25,94 @@
       setTimeout(removeStatusMessage, 3000);
     </script>
 </head>
-<body>
-   <h1>Products</h1>
+<body class="px-5 py-5">
 
-   <p>
-      <a href="{{route('product.create')}}" class="create-product-link">Create a Product</a>
-      <a href="{{route('product.index.exportcsv')}}" class="create-product-link">Export to CSV</a>
-   </p>
+   <div class="mx-auto max-w-4xl">
+      <h1 class="text-3xl">Products</h1>
 
-   <!-- pagination links - make sure controller is calling "Classname::paginate(X);"" -->
-   {{ $products->links() }}
+      <p class="mt-4 mb-4">
+         <a href="{{ route('product.create') }}" class="inline-block bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mr-3 text-xs uppercase font-bold">Create a Product</a>
+         <a href="{{ route('product.index.exportcsv') }}" class="inline-block bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded text-xs uppercase font-bold">Export to CSV</a>
+      </p>
 
-   {{-- output status message(s) if any --}}
-   @if (@session()->has('success'))
-   <div id="statusMessage" class="status-message success">
-      {{session('success')}}
+      <!-- pagination links - make sure controller is calling "Classname::paginate(X);"" -->
+      {{ $products->links() }}
+
+      {{-- output status message(s) if any --}}
+      @if (@session()->has('success'))
+      <div id="statusMessage" class="status-message success">
+         {{session('success')}}
+      </div>
+      @endif
+
+      <div class="mt-4">
+         <table class="w-full border-collapse rounded-md overflow-hidden text-sm">
+            <thead>
+               <tr class="bg-gray-200">
+                  <th class="py-2 px-3 font-bold border-b border-gray-400">ID</th>
+                  <th class="py-2 px-3 font-bold border-b border-gray-400">Name</th>
+                  <th class="py-2 px-3 font-bold border-b border-gray-400">Qty</th>
+                  <th class="py-2 px-3 font-bold border-b border-gray-400">Price</th>
+                  <th class="py-2 px-3 font-bold border-b border-gray-400">Description</th>
+                  <th class="py-2 px-3 font-bold border-b border-gray-400">Image</th>
+                  <th class="py-2 px-3 font-bold border-b border-gray-400">Edit</th>
+                  <th class="py-2 px-3 font-bold border-b border-gray-400">Delete</th>
+               </tr>
+            </thead>
+            <tbody id="table-body">
+            @foreach ($products as $index => $product)
+               <tr class="{{ $index % 2 === 0 ? 'bg-gray-100' : 'bg-white' }}">
+                  <td class="py-2 px-3 border-b border-gray-400">{{ $product->id }}</td>
+                  <td class="py-2 px-3 border-b border-gray-400">{{ $product->name }}</td>
+                  <td class="py-2 px-3 border-b border-gray-400">{{ $product->qty }}</td>
+                  <td class="py-2 px-3 border-b border-gray-400">{{ $product->price }}</td>
+                  <td class="py-2 px-3 border-b border-gray-400">{{ $product->description }}</td>
+                  <td class="py-2 px-3 border-b border-gray-400 text-center">
+                     @if ($product->thumbnail)
+                     <a href="/storage/{{ $product->image }}" target="_blank">
+                     <img src="/storage/{{ $product->thumbnail }}" width="50" height="50" alt="{{ $product->name }}">
+                     </a>
+                     @else
+                     <a href="/storage/{{ $product->image }}" target="_blank"><i class="fa-solid fa-image" aria-label="display image" title="Image opens in new tab/window"></i></a>
+                     @endif
+                  </td>
+                  <td class="py-2 px-3 border-b border-gray-400 text-center">
+                     <a href="{{ route('product.edit', ['product' => $product]) }}" class="inline-block bg-blue-500 hover:bg-blue-600 text-white py-1 px-3 rounded transition duration-300">Edit</a>
+                  </td>
+                  <td class="py-2 px-3 border-b border-gray-400 text-center">
+                     <form method="POST" action="{{ route('product.destroy', ['product' => $product]) }}">
+                     @csrf
+                     @method('delete')
+                     <button type="submit" class="inline-block bg-red-600 hover:bg-red-700 text-white py-1 px-3 rounded transition duration-300" onclick="return confirm('Delete {{ $product->name }}?')">Delete</button>
+                     </form>
+                  </td>
+               </tr>
+               @endforeach
+            </tbody>
+         </table>
+      </div>
+
+      <div class="mt-4 text-xs">
+         Current date/time: {{ date('Y-m-d H:i:s', time()) }}
+      </div>
+
+      <div class="flex flex-col items-center justify-center">
+         <div class="text-center text-xs uppercase text-gray-400">
+            Page Styled Using
+         </div>
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 54 33" class="w-10 h-10 mt-1">
+            <title>Tailwind CSS</title>
+            <g clip-path="url(#prefix__clip0)">
+               <path fill="#38bdf8" fill-rule="evenodd" d="M27 0c-7.2 0-11.7 3.6-13.5 10.8 2.7-3.6 5.85-4.95 9.45-4.05 2.054.513 3.522 2.004 5.147 3.653C30.744 13.09 33.808 16.2 40.5 16.2c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C36.756 3.11 33.692 0 27 0zM13.5 16.2C6.3 16.2 1.8 19.8 0 27c2.7-3.6 5.85-4.95 9.45-4.05 2.054.514 3.522 2.004 5.147 3.653C17.244 29.29 20.308 32.4 27 32.4c7.2 0 11.7-3.6 13.5-10.8-2.7 3.6-5.85 4.95-9.45 4.05-2.054-.513-3.522-2.004-5.147-3.653C23.256 19.31 20.192 16.2 13.5 16.2z" clip-rule="evenodd"/>
+            </g>
+            <defs>
+               <clipPath id="prefix__clip0">
+                  <path fill="#fff" d="M0 0h54v32.4H0z"/>
+               </clipPath>
+            </defs>
+         </svg>
+      </div>
    </div>
-   @endif
 
-   <div>
-      <table border="0">
-         <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Qty</th>
-            <th>Price</th>
-            <th>Description</th>
-            <th>Image</th>
-            <th>Edit</th>
-            <th>Delete</th>
-         </tr>
-         @foreach ($products as $product)
-         <tr>
-            <td>{{$product->id}}</td>
-            <td>{{$product->name}}</td>
-            <td>{{$product->qty}}</td>
-            <td>{{$product->price}}</td>
-            <td>{{$product->description}}</td>
-            <td align="center">
-               @if ($product->thumbnail)
-               <a href="/storage/{{$product->image}}" target="_blank">
-                  <img src="/storage/{{$product->thumbnail}}" width="50" height="50" alt="{{$product->name}}">
-               </a>
-               @else
-               <a href="/storage/{{$product->image}}" target="_blank"><i class="fa-solid fa-image" aria-label="display image" title="Image opens in new tab/window"></i></a>
-               @endif
-            </td>
-            <td align="center"><a href="{{route('product.edit', ['product' => $product])}}" class="button-like">Edit</a></td>
-            <td align="center">
-               <form method="POST" action="{{route('product.destroy', ['product' => $product])}}">
-                  @csrf
-                  @method('delete')
-                  <input type="submit" value="Delete" class="delete-button" onclick="return confirm('Delete {{$product->name}}?')">
-               </form>
-            </td>
-         </tr>
-         @endforeach
-      </table>
-   </div>
-
-   <p>
-      Current date/time: {{ date('Y-m-d H:i:s', time()) }}
-   </p>
 </body>
 </html>
