a1c8a02f7a7f8b40cee028d5a2ebff61bd5c076d
Francis Tsao <dialeleven@gmail.com>
2024-05-09 01:16:11 -0400

    removed old css link rel

--- app-crud/resources/views/products/index.blade.php
+++ app-crud/resources/views/products/index.blade.php
@@ -4,9 +4,6 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Product Index</title>
-   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-   <!-- Include Font Awesome CSS --> 
-   <link href="{{asset('assets/fontawesome/css/all.min.css')}}" rel="stylesheet" />
    @vite('resources/css/app.css')
    <script>
       // Function to remove the status message after a timeout
