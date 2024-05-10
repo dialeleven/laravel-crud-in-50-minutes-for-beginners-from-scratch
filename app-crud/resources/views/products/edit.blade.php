3321e1ee01739293a445862f7f03465d864951fb
Francis Tsao <dialeleven@gmail.com>
2024-05-10 07:15:59 -0400

    header

--- app-crud/resources/views/products/edit.blade.php
+++ app-crud/resources/views/products/edit.blade.php
@@ -3,7 +3,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Edit Product</title>
+   <title>CRUD App - Edit Product</title>
    @vite('resources/css/app.css')
 </head>
 
@@ -12,7 +12,7 @@
 <form method="post" action="{{route('product.update', ['product' => $product])}}" enctype="multipart/form-data" class="max-w-md mx-auto text-sm border p-4 rounded-lg shadow-md">
    @csrf
    @method('put')
-   
+
    <!-- header/logo -->
    @include('site.partials.header')
    
