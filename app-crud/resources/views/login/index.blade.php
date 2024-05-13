10efe2131fb7e6ed71bf7835a4820d1b773053ac
Francis Tsao <dialeleven@gmail.com>
2024-05-13 13:00:42 -0400

    removed @ svg

--- app-crud/resources/views/login/index.blade.php
+++ app-crud/resources/views/login/index.blade.php
@@ -35,22 +35,6 @@
         <div class="relative">
           <input id="email" name="email" type="email" autocomplete="email" required 
              class="w-full rounded-md p-4 pe-12 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-  
-          <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="size-4 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-              />
-            </svg>
           </span>
         </div>
       </div>
