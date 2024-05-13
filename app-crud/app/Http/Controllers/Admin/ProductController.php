dcc4183d652f284305505b58313d120a1beae7c8
Francis Tsao <dialeleven@gmail.com>
2024-05-13 14:05:43 -0400

    moved ProductController.php to Controllers\Admin. Adjustments to ProductController.php namespace, route namespace adjustment for ProductController path

--- app-crud/app/Http/Controllers/Admin/ProductController.php
+++ app-crud/app/Http/Controllers/Admin/ProductController.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller; // Import the base Controller class
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
--- app-crud/routes/web.php
+++ app-crud/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\Auth\LoginController;
 
 Route::get('/', function () {
