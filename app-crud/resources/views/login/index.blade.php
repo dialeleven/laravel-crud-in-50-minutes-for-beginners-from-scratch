f7a63e6d32ef014796bff138d946c907971f4204
Francis Tsao <dialeleven@gmail.com>
2024-05-13 12:48:11 -0400

    formatting

--- app-crud/resources/views/login/index.blade.php
+++ app-crud/resources/views/login/index.blade.php
@@ -4,7 +4,8 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Sign in</title>{{-- * @vite directive is for Vite.js to build Tailwind CSS/JS --}}
+   <title>Sign In</title>
+   {{-- * @vite directive is for Vite.js to build Tailwind CSS/JS --}}
    @vite('resources/css/app.css')
 </head>
 <body>
