1d92312e244413c03ec69a1c99125e9b6053fcc2
Francis Tsao <dialeleven@gmail.com>
2024-05-13 12:02:00 -0400

    Setup login controller/view. TODO: login model?

---
+++ app-crud/app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller; // Import the base Controller class
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
+
+use Illuminate\Support\Facades\DB;      // DB facade for traditional style SQL queries
+use Illuminate\Database\Eloquent\Model; // Eloquent ORM DB model
+
+use App\Models\Login;
+//use App\Models\User;
+
+
+class LoginController extends Controller
+{
+    /**
+     * login index
+     */
+    public function index() {
+        //return view('login.index', ['products' => $products]);
+        return view('login.index', []);
+    }
+    
+    /**
+     * * FOR TEST PURPOSES
+     */
+    public function test() {
+        //return view('products.index2', ['products' => $products]);
+        #$products = Product::all();
+        #$products = Product::paginate(5); // get paginated records
+
+        return view('products.test');
+    }
+}
--- app-crud/routes/web.php
+++ app-crud/routes/web.php
@@ -2,22 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Admin\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+//--------------- Product routes ---------------//
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-
-// ********** testing *************/
-Route::get('/product2', [ProductController::class, 'index2'])->name('product.index2');
-Route::get('/test', [ProductController::class, 'test'])->name('product.test');
-Route::get('/test2', [ProductController::class, 'test2'])->name('product.test2');
-// ********** /end testing *************/
-
 Route::get('/product/exportcsv', [ProductController::class, 'indexExportCsv'])->name('product.index.exportcsv');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
 Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
 Route::put('/product/{product}/update', [ProductController::class, 'update'])->name('product.update');
 Route::delete('/product/{product}/destroy', [ProductController::class, 'destroy'])->name('product.destroy');
+    // ********** testing *************/
+    Route::get('/product2', [ProductController::class, 'index2'])->name('product.index2');
+    Route::get('/test', [ProductController::class, 'test'])->name('product.test');
+    Route::get('/test2', [ProductController::class, 'test2'])->name('product.test2');
+    // ********** /end testing *************/
+//--------------- /Product routes ---------------//
+
+
+//--------------- Login routes ---------------//
+Route::get('/login', [LoginController::class, 'index'])->name('login.index');
+//--------------- /Login routes ---------------//
