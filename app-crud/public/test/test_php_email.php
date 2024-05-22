87c212a5298eab4c0808881ba813319ace9c5399
Francis Tsao <dialeleven@gmail.com>
2024-05-22 10:39:00 -0400

    cleanup

--- app-crud/public/test/test_php_email.php
+++ app-crud/public/test/test_php_email.php
@@ -1,8 +1,8 @@
 <?php
-$to_email = "getfrancistsao@gmail.com";
+$to_email = "username@gmail.com";
 $subject = "Simple Email Test via PHP from " . $_SERVER['HTTP_HOST'];
 $body = "Hi,\n\n This is test email send by PHP Script from " . $_SERVER['HTTP_HOST'];
-$headers = "From: getfrancistsao@gmail.com";
+$headers = "From: username@gmail.com";
  
 if (mail($to_email, $subject, $body, $headers)) {
     echo "Email successfully sent to $to_email... <h1>" . date("Y-m-d H:i:s") . "</h1>";
