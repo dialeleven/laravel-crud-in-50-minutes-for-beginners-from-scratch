cee478a85ab3315b4e9083bf87aea6913713828a
Francis Tsao <dialeleven@gmail.com>
2024-05-24 18:47:00 -0400

    first working PHPUnit test in Laravel - test_it_displays_the_index_page_with_products(). Had to add 'WithoutMiddleware' as trying to authenticate from the 'admins' table was causing all sorts of problems.

---
+++ app-crud/database/factories/AdminFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
+ */
+class AdminFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'username' => $this->faker->userName(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->email(),
+            'password' => $this->faker->password(),
+            'account_active' => $this->faker->boolean(),
+            'role_id' => $this->faker->numberBetween(1, 3),
+        ];
+    }
+}
---
+++ app-crud/database/factories/Common/ProductFactory.php
@@ -0,0 +1,33 @@
+<?php
+namespace Database\Factories\Common;
+
+use App\Models\Common\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+    
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name' => $this->faker->sentence(3),
+            'qty' => $this->faker->numberBetween(1, 100),
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            'description' => $this->faker->paragraph,
+            'datetime_in_stock' => $this->faker->dateTimeBetween('-1 year', 'now')->format('Y-m-d H:i:s'),
+            'image' => $this->faker->imageUrl(),
+            'thumbnail' => $this->faker->imageUrl(100, 100),
+        ];
+    }
+}
