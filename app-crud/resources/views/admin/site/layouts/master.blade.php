5e33139e30401ba4b4b06ab4b86f97f1e6c94ad7
Francis Tsao <dialeleven@gmail.com>
2024-05-28 15:23:48 -0400

    changed misc icon to box svg

--- app-crud/resources/views/admin/site/layouts/master.blade.php
+++ app-crud/resources/views/admin/site/layouts/master.blade.php
@@ -158,11 +158,11 @@
 
                <a href="{{route('admin-misc')}}" class="flex items-center px-3 py-2 text-blue-100 transition-colors duration-300 transform rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700
                hover:border-gray-500 dark:hover:border-gray-200 border border-transparent">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                      <title>Miscellany</title>
-                     <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
-                   </svg>
-                  <span class="mx-2 text-sm font-medium">Misc</span>
+                     <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+                  </svg>
+                  <span class="mx-2 text-sm font-medium">Misc.</span>            
                </a>
 
             </nav>
