41eadc6de84e4d4b15ca0a54435076961f173faf
Francis Tsao <dialeleven@gmail.com>
2024-05-27 21:09:06 -0400

    laravel-backup config

--- app-crud/config/database.php
+++ app-crud/config/database.php
@@ -43,8 +43,8 @@ return [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            'port' => env('DB_PORT', '3308'),
+            'database' => env('DB_DATABASE', 'app-crud'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
@@ -54,6 +54,16 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'dump' => [
+                'dump_binary_path' => 'G:\xampp8.2.4\mysql\bin',
+                /*
+                'excludeTables' => [
+                    
+                    'table_to_exclude_from_backup',
+                    'another_table_to_exclude',
+                ],
+                */
+            ],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
