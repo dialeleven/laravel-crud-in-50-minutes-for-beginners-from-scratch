fe8c594d88ac5787c4004214b90894356a329fe1
Francis Tsao <dialeleven@gmail.com>
2024-05-30 12:24:01 -0400

    finally sorted out testSendsResetLinkToAdmin().
    - adjusted 'use' path for 'App\Models\AdminModels\Admin' (missing 'AdminModels\')
    - role_id was missing in Admin::factory() - not sure if that was an issue
    - added other 'use' statements to resolve other phpunit errors

--- app-crud/routes/web.php
+++ app-crud/routes/web.php
@@ -219,14 +219,9 @@ Route::post('/admin-forgot-password/send-reset-link', function (Request $request
    // Log the status and check if notification is being sent
    #Log::info('status: ' . $status);
 
-   if ($status === Password::RESET_LINK_SENT) {
-      return back()->with('status', __('passwords.sent'));
-  } else {
-      return back()->withErrors(['email' => __($status)]);
-  }
-   // return $status === Password::RESET_LINK_SENT
-   //             ? back()->with(['status' => __($status)])
-   //             : back()->withErrors(['email' => __($status)]);
+   return $status === Password::RESET_LINK_SENT
+               ? back()->with(['status' => __($status)])
+               : back()->withErrors(['email' => __($status)]);
 })->middleware('auth:admin')->name('password.send-reset-link');
 
 
