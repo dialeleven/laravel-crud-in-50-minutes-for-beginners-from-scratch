93ef6094a31f96336016d357ce1eb481ca1148be
Francis Tsao <dialeleven@gmail.com>
2024-06-13 22:22:57 -0400

    front end public page. mobile nav is buggy currently.

--- app-crud/resources/views/public/index.blade.php
+++ app-crud/resources/views/public/index.blade.php
@@ -6,6 +6,7 @@
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <title>CRUD App - Public Home Page</title>
    @vite('resources/css/app.css')
+   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
 
@@ -22,8 +23,9 @@
       </a>
    
       <div class="flex flex-1 items-center justify-end md:justify-between">
-         <nav aria-label="Global" class="hidden md:block">
-         <ul class="flex items-center gap-6 text-sm">
+         <input type="checkbox" id="menu-toggle" class="peer hidden">
+         <nav aria-label="Global" class="hidden peer-checked:block md:block" id="mobile-menu">
+         <ul class="flex flex-col md:flex-row items-center gap-6 text-sm">
             <li>
                <a class="text-gray-500 transition hover:text-gray-500/75" href="#"> About </a>
             </li>
@@ -67,31 +69,68 @@
             </a>
          </div>
    
-         <button
-            class="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
-         >
+         <button class="block rounded-md bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden" id="menu-button">
+            <label for="menu-toggle">
             <span class="sr-only">Toggle menu</span>
             <svg
-               xmlns="http://www.w3.org/2000/svg"
+               aria-hidden="true"
                class="h-5 w-5"
                fill="none"
-               viewBox="0 0 24 24"
                stroke="currentColor"
-               stroke-width="2"
+               viewBox="0 0 24 24"
+               xmlns="http://www.w3.org/2000/svg"
             >
-               <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+               <path
+               stroke-linecap="round"
+               stroke-linejoin="round"
+               stroke-width="2"
+               d="M4 6h16M4 12h16M4 18h16"
+               ></path>
             </svg>
+            </label>
          </button>
          </div>
       </div>
    </div>
 </header>
 
-<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-   <h1 class="text-3xl font-bold tracking-tight">CRUD App - Home Page</h1>
 
-   Welcome to our CRUD App website!
-</div>
-
+<main>
+   <div class="relative px-6 lg:px-8">
+      <div class="mx-auto max-w-3xl pt-50 sm:pt-20 lg:pt-26">
+         <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+         <div class="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            <span class="text-gray-600"
+            ><a href="{{ route('public_products.index') }}" class="font-semibold text-indigo-600"
+               ><span class="absolute inset-0" aria-hidden="true"></span>New products available
+               <span aria-hidden="true">&rarr;</span></a
+            ></span>
+         </div>
+         </div>
+         <div>
+         <div class="text-center">
+            <h1 class="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
+               Your Product Title Here
+            </h1>
+            <p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+               A brief description about your product and what it does.
+            </p>
+            <div class="mt-8 flex gap-x-4 sm:justify-center">
+            <a
+               href="#"
+               class="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
+               >Get started</a
+            >
+            <a
+               href="#"
+               class="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+               >Learn more <span class="text-gray-400" aria-hidden="true">→</span></a
+            >
+            </div>
+         </div>
+         </div>
+      </div>
+   </div>
+</main>
 </body>
 </html>
